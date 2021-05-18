99ad200f988d72240beed5b4f6e06737d7e85c6c
Natanael Lima <taelima1997@gmail.com>
2021-05-17 23:20:59 -0300

    add log error fetch

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -5,7 +5,10 @@ import { api } from '../services/api';
 export default function Dashboard() {
   const { user } = useContext(AuthContext);
   useEffect(() => {
-    api.get('/me').then(response => console.log(response));
+    api
+      .get('/me')
+      .then(response => console.log(response))
+      .catch(error => console.log(error));
   }, []);
 
   return <h1>Dashboard: {user?.email}</h1>;
