a79e09271815f833ffe3a49352f80083a7ccd80b
Natanael Lima <taelima1997@gmail.com>
2021-05-25 14:31:20 -0300

    update with button signOut

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,12 +1,13 @@
 import { useContext, useEffect } from 'react';
-import { Can } from '../components/Can';
+
 import { AuthContext } from '../contexts/AuthContext';
 import { setupAPIClient } from '../services/api';
 import { api } from '../services/apiClient';
 import { withSSRAuth } from '../utils/withSSRAuth';
+import { Can } from '../components/Can';
 
 export default function Dashboard() {
-  const { user } = useContext(AuthContext);
+  const { user, signOut } = useContext(AuthContext);
 
   useEffect(() => {
     api.get('/me').then(response => console.log(response));
@@ -15,6 +16,10 @@ export default function Dashboard() {
   return (
     <>
       <h1>Dashboard: {user?.email}</h1>
+
+      <button type="button" onClick={signOut}>
+        Sign out
+      </button>
 
       <Can permissions={['metrics.list']}>
         <div>Métricas</div>
